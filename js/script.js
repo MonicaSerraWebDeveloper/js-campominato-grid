14240aaad0ac48559975df1eda9df19802c3e984
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-15 15:54:48 +0100

    feat: add the comment to understand better all the step

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
 // ed emetto un messaggio in console con il numero della cella cliccata.
 
+// Click sul pulsante "play" per far comparire la griglia con un display: block
+// Creare gli square con numeri da 1 a 100
+// Aggiungere gli square con i numeri dentro la griglia
+// Al click dell'utente dentro la cella questi colorano di azzurro il bg
+// Al click dell'utente viene stampato in console anche il numero corrispondente

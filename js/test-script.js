40ff71b2d90863b593f8aaaa987dfa2b86cf1fdd
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-15 22:11:42 +0100

    feat: test area add the function to create different cells based on different level

--- js/test-script.js
+++ js/test-script.js
@@ -2,7 +2,7 @@
 const gridGame = document.querySelector('.grid');
 const playStartTheGame = document.querySelector('.btn');
 
-const mySelect = document.querySelector('.select').value;
+const mySelect = document.getElementById('livello').value;
 console.log(mySelect);
 
 playStartTheGame.addEventListener('click', function () {
@@ -20,11 +20,34 @@ playStartTheGame.addEventListener('click', function () {
     }
 });
 
+// FUNCTIONS
+// La funzione ha lo scopo di generare delle celle chiamate square con numeri all'interno da 1 a 100
+// number -> è un numero 
+// return: l'elemento div che abbiamo creato a cui abbiamo aggiunto la classe .square con all'interno uno span e dentro lo span l'argomento number
 
 function squareGenerator(number) {
     let squareDiv = document.createElement('div');
     squareDiv.classList.add('square');
     squareDiv.innerHTML += `<span>${number}</span>`
     return squareDiv
+}
+
+
+// Funzione per generare un numero diverso di celle in base alla difficoltà di livello selezionata
+// numberCells -> un numero 
+// return: ritorna il numero di celle in base alla difficolta scelta nell'input select
+let howManyCells = difficultyLevel (mySelect)
+console.log(howManyCells);
+
+function difficultyLevel (input) {
+    let numberCells;
+    if (input === 'easy') {
+        numberCells = 100;
+    } else if (input === 'medium') {
+        numberCells = 81;
+    } else if (input === 'hard') {
+        numberCells = 49;
+    }
+    console.log(numberCells);
 }
 
